9f9d0eebd684e3f065ab456e58c0191351c6467f
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-07-02 14:32:54 +0600

    product page added to login testrunner

--- tests/LoginTestRunner.spec.js
+++ tests/LoginTestRunner.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require('@playwright/test');
 const LoginPage = require('../pages/LoginPage');
+const ProductPage = require('../pages/ProductPage');
 
 test('Login Test - Successful login with standard user', async ({ page }) => {
     const loginPage = new LoginPage(page); // Create an instance of the LoginPage class, passing the 'page' object.
@@ -7,4 +8,6 @@ test('Login Test - Successful login with standard user', async ({ page }) => {
     await expect(page).toHaveTitle(/Swag Labs/); // Expect a title "to contain" a substring.
     await loginPage.login('standard_user', 'secret_sauce'); // Call the 'login' method of the LoginPage instance, passing the username and password.
     await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html'); // Expect the URL to be 'https://www.saucedemo.com/inventory.html'
+    const productPage = new ProductPage(page);
+    await expect(productPage.products_label).toHaveText('Products'); // Expect the 'products_label' locator to have text 'Products'.
 });
