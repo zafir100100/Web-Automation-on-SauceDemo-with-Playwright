8e0bafd5386983f0c5c4f79215c327b3aa6fe942
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-07-02 14:24:24 +0600

    update for login page test runner

--- tests/LoginTestRunner.spec.js
+++ tests/LoginTestRunner.spec.js
@@ -6,6 +6,5 @@ test('Login Test - Successful login with standard user', async ({ page }) => {
     await loginPage.gotoLoginPage(); // Go to the login page using the 'gotoLoginPage' method.
     await expect(page).toHaveTitle(/Swag Labs/); // Expect a title "to contain" a substring.
     await loginPage.login('standard_user', 'secret_sauce'); // Call the 'login' method of the LoginPage instance, passing the username and password.
-    // Expect the URL to be 'https://www.saucedemo.com/inventory.html'
-    await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
+    await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html'); // Expect the URL to be 'https://www.saucedemo.com/inventory.html'
 });
