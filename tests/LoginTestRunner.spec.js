6611bebad1c3b159ba6d7fc877aa0a3457d32108
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-07-10 10:09:28 +0600

    read and initialize .env file variables

--- tests/LoginTestRunner.spec.js
+++ tests/LoginTestRunner.spec.js
@@ -1,6 +1,8 @@
 const { test, expect } = require('@playwright/test');
 const LoginPage = require('../pages/LoginPage');
 const ProductPage = require('../pages/ProductPage');
+require('dotenv').config() // read variables from .env file
+// console.log(process.env) // print all environment variables
 
 test('Login Test - Successful login with standard user', async ({ page }) => {
     const loginPage = new LoginPage(page); // Create an instance of the LoginPage class, passing the 'page' object.
@@ -17,7 +19,7 @@ test('Login Test - Successful login with standard user', async ({ page }) => {
 //     const loginPage = new LoginPage(page); // Create an instance of the LoginPage class, passing the 'page' object.
 //     await loginPage.gotoLoginPage(); // Go to the login page using the 'gotoLoginPage' method.
 //     await expect(page).toHaveTitle(/Swag Labs/); // Expect a title "to contain" a substring.
-//     await loginPage.login(process.env.USERNAME, process.env.PASSWORD); // Call the 'login' method of the LoginPage instance, passing the username and password.
+//     await loginPage.login(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD); // Call the 'login' method of the LoginPage instance, passing the username and password.
 //     await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html'); // Expect the URL to be 'https://www.saucedemo.com/inventory.html'
 //     const productPage = new ProductPage(page);
 //     await expect(productPage.products_label).toHaveText('Products'); // Expect the 'products_label' locator to have text 'Products'.
