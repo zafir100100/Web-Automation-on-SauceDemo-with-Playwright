ec2c4bc0d34cda41cfe2a750550403d4d6798853
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-07-10 09:09:14 +0600

    code format

--- pages/ProductPage.js
+++ pages/ProductPage.js
@@ -1,8 +1,8 @@
 class ProductPage {
-    constructor(page) {
-      this.page = page;
-      this.products_label = page.locator('[class="title"]');
-    }
+  constructor(page) {
+    this.page = page;
+    this.products_label = page.locator('[class="title"]');
   }
-  
-  module.exports = ProductPage;
+}
+
+module.exports = ProductPage;
